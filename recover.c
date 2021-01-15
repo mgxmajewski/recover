b008c631c22b3883b4267669dee4055a019e4bd7
mgxmajewski <mgxmajewski@gmail.com>
2021-01-15 21:58:16 +0000

    Add pseudocode

--- recover.c
+++ recover.c
@@ -3,5 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-
+    // Open memory card
+    // Repeat until end of card:
+        // Read 512 bytes into buffer
+        // If start of new JPEG
+            //If first JPEG
+            
+            // Else
+            
+        //Else
+            // If already found JPEG
+            
+    // Close any remaining files
 }
