6350c5a52525ef03d1ed3ff3343241aaa38f1011
mgxmajewski <mgxmajewski@gmail.com>
2021-01-16 22:52:04 +0000

    Add fread and declares name for it's 'size_t' return value

--- recover.c
+++ recover.c
@@ -10,28 +10,35 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    // Open memory card
+    
     if(argv[1] !=0)
     {
-    FILE *file = fopen(argv[1], "r");
-    
-
-    
-    // Repeat until end of card:
-    while(!feof(file))
-    {
-        continue;
-    }
-        // Read 512 bytes into buffer
-        // If start of new JPEG
-            //If first JPEG
+        FILE *file = fopen(argv[1], "r");
+        BYTE buffer[FILEBLOCK];
+        // Open memory card
+        size_t fread_byte;
+        
+        while(!feof(file))
+        {
+        // Repeat until end of card:
+            // Read 512 bytes into buffer
+            fread_byte = fread(buffer, sizeof(BYTE), FILEBLOCK, file);
+            // if (true)
+            // {
+            //     continue;
+            // }
+            // If start of new JPEG
+                //If first JPEG
+                
+                // Else
+                
+            //Else
+                // If already found JPEG
+                
+        // Close any remaining files
+            continue;
+        }
             
-            // Else
-            
-        //Else
-            // If already found JPEG
-            
-    // Close any remaining files
     }
     else
     {
