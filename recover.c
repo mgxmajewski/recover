dcd4e5e4a0fc7ff719a5820021511f2e336f1762
mgxmajewski <mgxmajewski@gmail.com>
2021-01-17 17:09:45 +0000

    Declare recovered_img_count and recovered_filename

--- recover.c
+++ recover.c
@@ -10,39 +10,36 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    
+
     if(argv[1] !=0)
     {
-        FILE *file = fopen(argv[1], "r");
+        //Declare buffer size
         BYTE buffer[FILEBLOCK];
-        // Open memory card
-        size_t fread_byte;
+        int recovered_img_count =0;
+        char recovered_filename[8];
         
-        while(!feof(file))
+        FILE *recovery_file = fopen(argv[1], "r");
+        // Open memory card
+        while(!feof(recovery_file))
         {
         // Repeat until end of card:
             // Read 512 bytes into buffer
-            fread_byte = fread(buffer, sizeof(BYTE), FILEBLOCK, file);
-          
+            size_t fread_byte;
+            fread_byte = fread(buffer, sizeof(BYTE), FILEBLOCK, recovery_file);
+
             // If start of new JPEG
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 ) == 0xe0)
             {
                 //If first JPEG
-                fwrite(buffer, sizeof(BYTE), fread_byte, file);
+              
                 // Else
-                continue;
             }
-                //If first JPEG
-                
-                // Else
-                
             //Else
                 // If already found JPEG
-                
+
         // Close any remaining files
-            continue;
         }
-            
+
     }
     else
     {
