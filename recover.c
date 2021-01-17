48e3b0cfc37b131135b73aece1002ff1151d5590
mgxmajewski <mgxmajewski@gmail.com>
2021-01-17 18:32:36 +0000

    Include stdbool.h and delacres frist_jpg bool

--- recover.c
+++ recover.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <stdbool.h>
 #define FILEBLOCK 512
 typedef uint8_t BYTE;
 
@@ -17,20 +17,20 @@ int main(int argc, char *argv[])
         BYTE buffer[FILEBLOCK];
         int recovered_img_count =0;
         char recovered_filename[8];
-        
+        bool first_jpg = false;
         FILE *analized_file = fopen(argv[1], "r");
-        // Open memory card
+        FILE *img = NULL;
         
+        // Open memory card
         size_t fread_byte;
         fread_byte = fread(buffer, FILEBLOCK, 1, analized_file);
-        
-        while(!feof(analized_file))
+
+        while(fread_byte == 1)
         {
         // Repeat until end of card:
             // Read 512 bytes into buffer
-            FILE *img = NULL;
-           
-
+            
+         
             // If start of new JPEG
             if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 ) == 0xe0)
             {
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
                 img = fopen(recovered_filename, "w");
                 recovered_img_count++;
             }
-            
+               
             //Else
                 // If already found JPEG
 
